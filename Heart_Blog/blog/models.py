7c1715e1be3b92b302ebd5115a5ea8c9a6a41d6b
Azzeddine Dali <azzeddinedali@outlook.com>
2020-11-12 20:05:15 +0100

    Adding base, detail and home template

--- Heart_Blog/blog/models.py
+++ Heart_Blog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogArticle(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog-pics')
+
+    def __str__(self):
+        return self.title
--- Heart_Blog/blog/views.py
+++ Heart_Blog/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import BlogArticle
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+
+    articles = BlogArticle.objects.all()
+    return render(request, 'blog/home.html', {"blogs": articles})
+
+
+def detail(request):
+    return render(request, 'blog/detail.html')
