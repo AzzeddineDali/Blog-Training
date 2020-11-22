51e7aaf53a17f01d0b60d732e3b4d76df5dd4a32
Azzeddine Dali <azzeddinedali@outlook.com>
2020-11-22 19:49:05 +0100

    Adding Update Template

--- Heart_Blog/blog/urls.py
+++ Heart_Blog/blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
-from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostDeleteView,
+    PostUpdateView,
+)
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home-page'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/create/', PostCreateView.as_view(), name='post-create')
+    path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
 ]
--- Heart_Blog/blog/views.py
+++ Heart_Blog/blog/views.py
@@ -1,20 +1,43 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from .models import BlogArticle
-from django.views.generic import ListView, DetailView, CreateView
-from .models import BlogArticle
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+)
 
 
+# Show all posts
 class PostListView(ListView):
     model = BlogArticle
     template_name = 'blog/home.html'
     context_object_name = 'blogs'
 
 
+# Show one post
 class PostDetailView(DetailView):
     model = BlogArticle
 
 
+# Create a new post
 class PostCreateView(CreateView):
     model = BlogArticle
     fields = ['title', 'description', 'image']
-    
+    success_url = reverse_lazy('home-page')
+
+
+# Update Post
+class PostUpdateView(UpdateView):
+    model = BlogArticle
+    fields = ['title', 'description']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('home-page')
+
+
+# Delete a post
+class PostDeleteView(DeleteView):
+    model = BlogArticle
+    success_url = reverse_lazy('home-page')
